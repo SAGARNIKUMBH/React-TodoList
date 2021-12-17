60813625defaa5e18ef31bae41cb7f7a04b6c4b5
SAGAR_NIKUMBH <sagarnikumbh.weoto1991@gmail.com>
2021-12-17 11:40:57 +0530

    New React Project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        The Value of Personal Statements Have you considered the full value your
+        Personal Statement adds to strengthen your overall medical residency
+        application? Some residency applicants are not aware of the many roles a
+        Personal Statement plays in the residency application process from
+        interview selection to Rank Order Lists. The Medical Residency Personal
+        Statement can boost a weak application past residency program filters,
+        secure an interview, provide content for program interview questions,
+        and even help Program Directors make the final selections for their Rank
+        Order List. A well-crafted Personal Statement is crucial to help you
+        stand out from the thousands of other applicants contending for limited
+        residency program positions. Residency Statement will provide you with
+        the Personal Statement you need to guide you along your ERAS Residency
+        Application journey with strength and confidence.
+      </p>
+    </div>
+  );
+};
